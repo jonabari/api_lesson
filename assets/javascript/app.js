b4c3375c748cc7379284bd01feba82bc95d6e8d4
jonabari <barcelo.jonathan@gmail.com>
2018-10-19 13:25:08 -0500

    "Update: api post method not working"

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,66 @@
-var key = "1ae2d602-e0b0-4458-a2e7-86954a872f0e";
-var queryURL = "http://version1.api.memegenerator.net//Generators_Search?q=" + query + "&pageIndex=0&pageSize=12&apiKey=" + key;
-var query = "dog";
+//Global vars
+var generatorID
+var imageID
+var imageURL
+var memefied
+var captionURL
 
-$ .ajax ( {
-    url: queryURL ,
-    method: "GET"
-} ) .then ( function ( response ) {
-    console.log(response);
+//STEP 1: Finad a meme
+var search = function() {
+    $( "#search-results" ).empty();
+    var queryURL = "https://api.imgflip.com/get_memes"; 
+    $ .ajax ({
+        url: queryURL ,
+        method: "GET"
+    }) .then ( function ( response ) {
+        console.log( response );
+        for ( let i = 0 ; response.data.memes.length ; i++ ) {
+            let meme = $( "<img>" ).attr( "src", response.data.memes[ i ].url );
+            meme.attr( "data-id", response.data.memes[ i ].id);
+            meme.addClass("img-fluid")
+            meme.addClass("meme-img") 
+            $( "#search-results" ).append( meme )
+        };
+    });
+};
+
+search(); //meme templates are pulled on page load
+
+$( document ).on( "click", ".meme-img", function () {
+    $("#final-img").empty();
+    imageID = $(this).attr( "data-id" )
+    console.log( 'image ID ====>', imageID )
+    imageURL = $(this).attr( "src" )
+    console.log( 'image URL ====>', imageURL )
+    memefied = $( "<img>" ).attr( "src", imageURL )
+    memefied.addClass( "img-fluid" )
+    memefied.attr( "id", "meme-final" ) 
+    memefied.attr( "data-id", imageID ) ;
+    $( "#final-img" ).append( memefied )
 });
+
+
+//STEP 2: Add some TOP and BOTTOM text
+$( document ).on( "click", "#push-btn", function () {
+    let topText = $("#top-box").val().trim()
+    console.log ("TOP ===>", topText)
+    let botText = $("#bot-box").val().trim()
+    console.log ("BOTTOM ===>", botText)
+
+    captionUrl = `https://api.imgflip.com/caption_image` 
+    $ .ajax ({
+        url: captionURL ,
+        method: "POST" ,
+        data: { 
+            template_id: imageID, 
+            username: "jonabar",
+            password: "gatoPerr0caballoconejo",
+            text0: topText,
+            text1: botText,
+        }
+
+    }) .then ( function ( response ) {
+        console.log(response)
+        // memefied.attr( "src", response.result.instanceImageUrl )
+    })
+})
